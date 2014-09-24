802f5c9d771b3da08833c34c09f3cc8d45de70c6
Sier Violencia <sierisimo@gmail.com>
2014-09-24 00:53:05 -0500

    Check of length on T(m,n) for arguments - Added comments for documentation

--- URM/Operations.js
+++ URM/Operations.js
@@ -28,74 +28,108 @@ var Operations = {
    *   Resets the value on register "n" to 0
    */
   Z: function(s){
+    //At first, s is a string, that's why I send it to check for his "realValue"
     var n = realVal(s);
+    //After getting the real value, check the result
     if(n === -1 || (typeof n !== 'number')){
       debug("Position isn't a number");
       throw new Error(errorStr);
     }
-
+    /*
+     * If "n" is bigger than the actual size of the registers, it means that we need to mark as zero
+     * every register from actual length to "n" element
+     */
     if((n-1) > this.registers.length){
       for(var i = this.registers.length; i<(n-1);i++){
         this.registers.push(0);
       }
     }
-
+    //Set as zero the n register (user sees the register as n, for us is n-1 because the reigster start at 0)
     this.registers[n-1] = 0;
     this.position++;
   },
+  /*
+   * S(n):
+   *    Adds one to the register in n
+   */
   S: function(s){
+    //At first, s is a string, that's why I send it to check for his "realValue"
     var n = realVal(s);
 
     if(n === -1 || (typeof n !== 'number')){
       debug("Position isn't a number");
       throw new Error(errorStr);
     }
-
+    /*
+     * If "n" is bigger than the actual size of the registers, it means that we need to mark as zero
+     * every register from actual length to "n" element
+     */
     if((n-1) > this.registers.length){
       for(var i = this.registers.length; i<=(n-1);i++){
         this.registers.push(0);
       }
     }
-
+    //Add one to register n. Remember, for us: n is equals to n-1, because we started at position 0
     this.registers[n-1]++;
     this.position++;
   },
+  /*
+   * T(m,n):
+   *    Copies the value on register "m" to register "n".
+   */
   T: function(s){
     var n = realVal(s);
-
-    if(n === -1 || !(n instanceof Array)){
-      debug("Arguments aren't correct or neither a group of numbers");
+    // IF n is -1 some error happened parsing the string.
+    //Also if it isn't an array containing [m,n] is a mistake on parsing
+    if(n === -1 || !(n instanceof Array) || n.length !== 2){
+      debug("Arguments aren't correct or neither a group of 2 numbers");
       throw new Error("Arguments for operation are malformed, must be: T(number1,number2)");
     }
-
+    //Asing positions on our "registers"
     this.registers[n[1]-1] = this.registers[n[0]-1];
     this.position++;
   }
 };
 
+/*
+ * realVal(str):
+ *    It needs a string for works, then it tries to check for form of the string:
+ *        - "x" just an element:
+ *            a) Is a character (or a group of characters) that can be parsed as numbers. returns the value parsed
+ *            b) not a character representing a number, returns -1
+ *
+ *        - "x,y" two elements divided by a comma:
+ *            *) checks for x and y to be characters that represents numbers, if they are, returns [x,y] else returns -1
+ *
+ *        - "x,y,z" three elements divided by a comma:
+ *            *) checks for x, y and z, to be characters that represents numbers, if they are, returns [x,y,z] else returns -1
+ *
+ */
 function realVal(str){
   var numbers = str.split(","),
       value;
-
+  //Not elments on string. ergo: str === ""
   if(numbers.length === 0){
     return -1;
   }
 
+  //numbers is an array of strings, where each element is a string that was divided from other
+  // string by a comma
   switch(numbers.length){
     case 1:
       value = new Number(numbers[0]).valueOf();
+      //check if it is a real number
       return (Number.isNaN(value)?-1:value);
-    case 2:
+    default:
       for(var i = numbers.length, l = i; l--;){
         var tmpN = new Number(numbers[l]).valueOf();
+        //if "x" or "y" isn't a number, reutnr -1
         if(Number.isNaN(tmpN)){
           return -1;
         }
         numbers[l] = tmpN;
       }
       return numbers;
-    case 3:
-      break;
   }
 }
 
